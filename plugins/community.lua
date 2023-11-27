35b85c8e8ec2816c1ddc74fe5a2b959c7b73f741
UserCommon <kirilnenashev@gmail.com>
2023-11-28 00:02:14 +0300

    0.0.1 plugin

---
+++ plugins/community.lua
@@ -0,0 +1,12 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.haskell" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.java" },
+}
